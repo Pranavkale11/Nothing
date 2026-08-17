4d6b1f1f358e7a91bfdc6139b34935b3d7197d69
Pranav Kale Jain <pranavkale3011@gmail.com>
2026-08-17 19:45:05 +0530

    Create facts.js

---
+++ facts.js
@@ -0,0 +1,8 @@
+const funFacts = [
+  "Honey never spoils.",
+  "Bananas are berries, but strawberries aren't.",
+  "Octopuses have three hearts.",
+  "A day on Venus is longer than a year on Venus."
+];
+
+console.log(funFacts[Math.floor(Math.random() * funFacts.length)]);
